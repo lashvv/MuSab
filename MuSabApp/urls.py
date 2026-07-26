de2333c58fce3ddf96ae90d39f0fa61a8478405f
Lasha <lashajinchar.adze16@gmail.com>
2026-07-26 19:58:02 +0400

    Add page templates and fix index page links

--- MuSabApp/urls.py
+++ MuSabApp/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('album/<int:album_id>/', views.album_detail, name='album'),
+    path('timeline/', views.timeline, name='timeline'),
+    path('discover/', views.discover, name='discover'),
 ]
--- MuSabApp/views.py
+++ MuSabApp/views.py
@@ -6,3 +6,15 @@ def index(request):
     albums = Album.objects.all()
 
     return render(request, 'MuSabApp/index.html', {'title': 'Home', 'albums': albums})
+
+def album_detail(request, album_id):
+    album = Album.objects.get(id=album_id)
+    return render(request, 'MuSabApp/album.html', {'title': album.album, 'album': album})
+
+def timeline(request):
+    albums = Album.objects.all().order_by('year')
+    return render(request, 'MuSabApp/timeline.html', {'title': 'Timeline', 'albums': albums})
+
+def discover(request):
+    albums = Album.objects.all()
+    return render(request, 'MuSabApp/discover.html', {'title': 'Discover', 'albums': albums})
